3f8853b76b17ce1d698b83d93d29d07f1b46e453
Leon Li <la2li@uwaterloo.ca>
2015-01-17 17:18:47 -0500

    generated angular app

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name nbtc2015App
+ * @description
+ * # nbtc2015App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('nbtc2015App', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nbtc2015App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nbtc2015App
+ */
+angular.module('nbtc2015App')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
