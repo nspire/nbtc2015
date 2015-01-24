033a79682ef3fe2719745089bd3a5c7cb2e2c9fe
Leon Li <la2li@uwaterloo.ca>
2015-01-24 16:27:48 -0500

    Added contact route

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nbtc2015App.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the nbtc2015App
+ */
+angular.module('nbtc2015App')
+  .controller('ContactCtrl', function ($scope) {
+  });
---
+++ test/spec/controllers/contact.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe('Controller: ContactCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('nbtc2015App'));
+
+  var ContactCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    ContactCtrl = $controller('ContactCtrl', {
+      $scope: scope
+    });
+  }));
+});
