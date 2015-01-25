385fb71ed0989c568fdbf86f91297c9dc3939d3c
Leon Li <la2li@uwaterloo.ca>
2015-01-24 20:15:09 -0500

    Initial contact page implementation

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -9,4 +9,22 @@
  */
 angular.module('nbtc2015App')
   .controller('ContactCtrl', function ($scope) {
+    $scope.connectLinks = [
+      {
+        icon: 'fa-youtube',
+        url: 'https://www.youtube.com/user/NspireInnovationNet'
+      },
+      {
+        icon: 'fa-twitter',
+        url: 'https://twitter.com/NspireNetwork'
+      },
+      {
+        icon: 'fa-facebook',
+        url: 'https://www.facebook.com/NspireNetwork'
+      },
+      {
+        icon: 'fa-linkedin',
+        url: 'https://ca.linkedin.com/company/nspire-innovation-network'
+      },
+    ];
   });
