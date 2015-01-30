819298905be717b14d3f3f54b83b40ebbd52b4af
Leon Li <la2li@uwaterloo.ca>
2015-01-29 23:57:59 -0500

    Add blurb service

---
+++ app/scripts/services/blurbservice.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name nbtc2015App.blurbService
+ * @description
+ * # blurbService
+ * Service in the nbtc2015App.
+ */
+angular.module('nbtc2015App')
+  .service('blurbService', function ($sce) {
+
+    var service = {
+      // 'content' object definition is flexible
+      // the only data needed to make this service work is 'content.blurb'
+      content: {},
+      isOpen: false,
+      show: function (content) {
+        service.isOpen = true;
+        // trust blurb html & allow template usage
+        if (content && content.blurb && (typeof content.blurb === 'string')) {
+          content.blurb = $sce.trustAsHtml(content.blurb);
+          console.log(content.blurb);
+        }
+        service.content = content ? content : {};
+      },
+      hide: function () {
+        service.isOpen = false;
+      }
+    };
+
+    return service;
+  });
---
+++ test/spec/services/blurbservice.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('Service: blurbService', function () {
+
+  // load the service's module
+  beforeEach(module('nbtc2015App'));
+
+  // instantiate service
+  var blurbService;
+  beforeEach(inject(function (_blurbService_) {
+    blurbService = _blurbService_;
+  }));
+
+});
